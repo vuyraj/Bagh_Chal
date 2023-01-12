e0b0f7e9fc65d92a02270de8fee2371824302757
Yuvraj Sah <60791717+vuyraj@users.noreply.github.com>
2023-01-12 10:44:49 +0545

    Update main.py

--- baghchal/main.py
+++ baghchal/main.py
@@ -1,5 +1,27 @@
 import pygame
 from baghchal.constants import HEIGHT,WIDTH
+from baghchal.board import Board
 
 WIN = pygame.display.set_mode(WIDTH,HEIGHT)
 pygame.display.set_caption('!!! BAGH  CHAL !!! ')
+
+def main():
+    run = True
+    clock = pygame.time.Clock()  
+    board = Board()
+
+    while run:
+        clock.tick(FPS)
+   
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pass
+
+        board.draw(WIN)
+        pygame.display.update()       
+    pygame.quit()
+
+main()
